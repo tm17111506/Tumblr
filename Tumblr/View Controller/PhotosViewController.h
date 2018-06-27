dc3dbe7b5532c3ce8f7f6e17802deffd6b7f4b31
Tiffany Ma <tma@fb.com>
2018-06-27 14:22:35 -0700

    Table view (not table view cell)

---
+++ Tumblr/View Controller/PhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.h
+//  Tumblr
+//
+//  Created by Tiffany Ma on 6/27/18.
+//  Copyright © 2018 Tiffany Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ Tumblr/View Controller/PhotosViewController.m
@@ -0,0 +1,71 @@
+//
+//  PhotosViewController.m
+//  Tumblr
+//
+//  Created by Tiffany Ma on 6/27/18.
+//  Copyright © 2018 Tiffany Ma. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *posts;
+@end
+
+@implementation PhotosViewController
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.posts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = [NSString stringWithFormat:@"This is row %ld", (long)indexPath.row];
+    
+    return cell;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSLog(@"%@", dataDictionary);
+            // TODO: Get the posts and store in posts property
+            NSDictionary *responseDictionary = dataDictionary[@"response"];
+            self.posts = responseDictionary[@"posts"];
+            // TODO: Reload the table view
+        }
+    }];
+    [task resume];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
